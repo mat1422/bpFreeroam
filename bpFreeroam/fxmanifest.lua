daa0b3768b8500a87c518063297f8e35e2c046b6
Bape_off <84997746+bape69-oss@users.noreply.github.com>
2022-01-11 22:56:11 +0100

    Add files via upload

---
+++ bpFreeroam/fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'cerulean'
+games { 'gta5' };
+
+name 'Menu FreeRoam (RageUI V2)';
+author 'Bape 🐕#9021';
+
+client_scripts {
+    "src/RMenu.lua",
+    "src/menu/RageUI.lua",
+    "src/menu/Menu.lua",
+    "src/menu/MenuController.lua",
+    "src/components/*.lua",
+    "src/menu/elements/*.lua",
+    "src/menu/items/*.lua",
+    "src/menu/panels/*.lua",
+    "src/menu/windows/*.lua",
+
+
+    "cl_freeroam.lua",
+    "cfg_freeroam.lua"
+}
+
+server_scripts {
+    "cfg_freeroam.lua"
+}
+
+shared_script '@es_extended/imports.lua'
